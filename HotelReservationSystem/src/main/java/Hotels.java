369cb3548f7beafdb098af8d7bc8d80d63489762
AdeshMask <adeshmaske255@gmail.com>
2022-04-18 10:13:05 +0530

    Added:Hotel name and rates

---
+++ HotelReservationSystem/src/main/java/Hotels.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Hotels {
+    Rates rates = new Rates();
+    Scanner scanner = new Scanner(System.in);
+    HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+
+    public void lakeWood(){
+        System.out.println("Welcome to lakeWood hotel:");
+        System.out.println("Enter Customer type: ");
+        int ch = scanner.nextInt();
+        if (ch == 1){
+            hotelReservationSystem.week_Day_Regualr_Rates = 110;
+            hotelReservationSystem.week_Day_Rewards_Rates = 80;
+            regularRates(hotelReservationSystem.week_Day_Regualr_Rates,hotelReservationSystem.week_Day_Rewards_Rates);
+        }
+        else {
+            hotelReservationSystem.week_End_Regular_Rates = 90;
+            hotelReservationSystem.week_End_Rewards_Rates = 80;
+            rewardsrates(hotelReservationSystem.week_End_Regular_Rates, hotelReservationSystem.week_End_Rewards_Rates);
+        }
+    }
+    public void bridgewood() {
+        System.out.println("Welcome to Bridgewood Hotel");
+        System.out.println("Welcome to lakeWood hotel:");
+        System.out.println("Enter Customer type: ");
+        int ch1 = scanner.nextInt();
+        if (ch1 == 1){
+            hotelReservationSystem.week_Day_Regualr_Rates = 160;
+            hotelReservationSystem.week_Day_Rewards_Rates = 110;
+            regularRates(hotelReservationSystem.week_Day_Regualr_Rates,hotelReservationSystem.week_Day_Rewards_Rates);
+        }
+        else {
+            hotelReservationSystem.week_End_Regular_Rates = 60;
+            hotelReservationSystem.week_End_Rewards_Rates = 50;
+            rewardsrates(hotelReservationSystem.week_End_Regular_Rates, hotelReservationSystem.week_End_Rewards_Rates);
+        }
+    }
+    public void ridgewood() {
+        System.out.println("Welcome to Ridgewood Hotel");
+    }
+    public void regularRates(int rate1,int rate2){
+        System.out.println("Rate for the Regular customer are on Weekdays: "+rate1);
+        System.out.println("Rate for the Regular custome on Weekend are: "+rate2);
+    }
+    public void rewardsrates(int rate1, int rate2){
+        System.out.println("Rate for the Rewards customer are on Weekdays: "+rate1);
+        System.out.println("Rate for the Rewards custome on Weekend are: "+rate2);
+    }
+}
---
+++ HotelReservationSystem/src/main/java/Rates.java
@@ -0,0 +1,26 @@
+public class Rates {
+
+    public static int week_Day_Regualr_Rates;
+    public static int week_Day_Rewards_Rates;
+    public static int week_End_Regular_Rates;
+    public static int week_End_Rewards_Rates;
+    void lakewood(){
+        this.week_Day_Regualr_Rates = 110;
+        this.week_Day_Rewards_Rates = 80;
+        this.week_End_Regular_Rates = 90;
+        this.week_End_Rewards_Rates = 80;
+    }
+    void bridgeWood(){
+        this.week_Day_Regualr_Rates = 160;
+        this.week_Day_Rewards_Rates = 110;
+        this.week_End_Regular_Rates = 60;
+        this.week_End_Rewards_Rates = 50;
+    }
+
+    void ridgeWood(){
+        this.week_Day_Regualr_Rates = 220;
+        this.week_Day_Rewards_Rates = 100;
+        this.week_End_Regular_Rates = 150;
+        this.week_End_Rewards_Rates = 40;
+    }
+}
